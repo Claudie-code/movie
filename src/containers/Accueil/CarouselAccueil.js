17d141e8b4fdc5e0ead7d8e1a6e3bda6eb81b2b4
Claudie-code <65274376+Claudie-code@users.noreply.github.com>
2021-09-20 19:54:11 +0200

    css carousel component linear gradient background

--- src/containers/Accueil/CarouselAccueil.js
+++ src/containers/Accueil/CarouselAccueil.js
@@ -16,18 +16,20 @@ function CarouselAccueil({ popularMovies }) {
           <Title>Populaires en ce moment</Title>
             <Carousel >
               {popularMovies && popularMovies.slice(0, 3).map(popularMovie => (
-                <Carousel.Item key={popularMovie.id}>
+                <Carousel.Item key={popularMovie.id}  style={{position: "relative", width:"100%"}}>
                   <Link to={`/movie/${popularMovie.id}`}>
                     <img
-                      className="d-block w-100"
                       src={`https://image.tmdb.org/t/p/w${taille}${popularMovie.backdrop_path}`}
                       alt={popularMovie.title}
-                    />
+                    >
+                      
+                    </img>
+                    <div style={{position:"absolute", width:"100%", height:"100%",top:0, background: "linear-gradient(rgba(0,0,0,0) 0%, rgba(0,0,0,0.15) 50%, rgba(0,0,0,.45) 75%, rgba(0,0,0,.60) 85%, rgba(0,0,0,.75) 90%, rgba(0,0,0,0.90) 100%)"}}></div>
                   </Link>
                   <Carousel.Caption>
                     <Typography variant="h3" gutterBottom>{popularMovie.title}</Typography>
                     <Typography variant="body1" gutterBottom>
-                      {popularMovie.overview} <br/> Sortie le {popularMovie.release_date}
+                      {popularMovie.overview.slice(0,200) + "..."} <br/> Sortie le {popularMovie.release_date}
                     </Typography>
                   </Carousel.Caption>
                 </Carousel.Item>
