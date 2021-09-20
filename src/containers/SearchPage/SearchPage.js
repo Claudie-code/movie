a1d46679714878273d9cb74e0457c2ae08664df4
Claudie-code <65274376+Claudie-code@users.noreply.github.com>
2021-09-20 20:08:09 +0200

    fix result api data searchPage

--- src/containers/SearchPage/SearchPage.js
+++ src/containers/SearchPage/SearchPage.js
@@ -5,21 +5,27 @@ import { Grid } from '@material-ui/core';
 import { useApiData } from '../../hooks/useApiData';
 import MovieCardList from '../GenrePage/MovieCardList';
 import { useParams } from "react-router-dom";
+import Loader from '../../components/Loader/Loader';
 
 export default function SearchPage() {
     const { search } = useParams();
     const [page, setPage] = useState(1);
-    const searchResults = useApiData(`
+    const [ searchResults, loading ] = useApiData(`
         https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_THEMOVIEDB_KEY}&language=fr-FR&query=${search}&page=${page}&include_adult=false`, search, page
     );
-
+    console.log(searchResults)
     return (
         <>
-            <Title>Résultats pour {search}</Title>
-                <Grid container justifyContent="center" spacing={3}>
-                    {searchResults && <MovieCardList movies={searchResults} />}
-                    <Pagination count={10} page={page} onChange={(event, value) => setPage(value)} />
-                </Grid>
+            {loading ? 
+                <Loader /> :
+                <>
+                    <Title>Résultats pour {search}</Title>
+                    <Grid container justifyContent="center" spacing={3}>
+                        {searchResults && <MovieCardList movies={searchResults} />}
+                        <Pagination count={10} page={page} onChange={(event, value) => setPage(value)} />
+                    </Grid>
+                </>
+            }
         </>
     );
 }
